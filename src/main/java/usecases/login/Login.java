3b179d655a5475288502127b7582c9161e9e4089
aryangoel24 <90288650+aryangoel24@users.noreply.github.com>
2022-12-07 18:34:51 -0500

    Revert "LogicCodeTest and LoginTest"

--- src/main/java/usecases/login/Login.java
+++ src/main/java/usecases/login/Login.java
@@ -11,12 +11,12 @@ public class Login {
             if (password.equals(correctPassword)){
                 return 2;
             } else {
-                // incorrect Password
+                // final submission on gui should be able to say "Incorrect Password"
                 return 1;
             }
         } else{
-            // user doesn't exist
             return 0;
+            //System.out.println("username doesn't exist");
         }
     }
 
